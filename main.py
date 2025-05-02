745a91223b7cdf1da23f36673f3a65d74653ece3
Mahmed1109 <m.ahmed110900@gmail.com>
2025-05-02 10:13:30 +0100

    function for retriving meal

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ def find_meal_by_ingredient(ingredient):
     response= requests.get(url, params=query)
     data= response.json()
     return data.get("meals",[])
+
+def get_meal(meal_id):
+    url=f"{API_url}/lookup.php"
+    query={"i": meal_id}
+    response=requests.get(url,params=query)
+    data=response.json()
+    return data["meals"][0] if data["meals"] else None
