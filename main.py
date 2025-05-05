dfbc0a928eca1ee0d688cf8c54e38533f28705d9
Mahmed1109 <m.ahmed110900@gmail.com>
2025-05-05 10:10:12 +0100

    Display meals

--- main.py
+++ main.py
@@ -15,3 +15,12 @@ def get_meal(meal_id):
     response=requests.get(url,params=query)
     data=response.json()
     return data["meals"][0] if data["meals"] else None
+
+def display_meal(meal):
+    print(f"\n {meal['strMeal']}")
+    print(f"Category: {meal['strCategory']}")
+    print(f"Area: {meal['strArea']}")
+    print(f"Instructions:\n{meal['strInstructions'][500]}...")
+    print(f"Image: {meal['strMealThumb']}")
+    print(f"Video Tutorial: {meal['strYoutube']}\n")
+    
